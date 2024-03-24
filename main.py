b67f1185379860c3be3017cac8128d3720e64a5e
pmrd <parhammrd@gmail.com>
2024-03-24 13:33:23 +0330

    add logging

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import logging
 from fastapi import FastAPI, Response
 
 
 app = FastAPI()
 
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
+
 
 @app.get('/')
 def root():
+    logger.info('Attempting to get root direction')
     return Response('<h1>Interactive film API.</h1>')
